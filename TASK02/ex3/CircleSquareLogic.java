b36ae58d7e67ddb25efb84f7ebdc31c1fc24feb3
Vladislav <36796951+vladstepway@users.noreply.github.com>
2018-05-26 22:26:32 +0300

    edited 3.14 tp Math.PI

--- TASK02/ex3/CircleSquareLogic.java
+++ TASK02/ex3/CircleSquareLogic.java
@@ -1,11 +1,11 @@
 package by.stepovoy.ex3;
 
-public class CircleSquareLogic {
+public class  CircleSquareLogic {
 
     public static double circleSquare(double outRadius, double inRadius) {
 
-        return outRadius > inRadius ? 3.14 * (Math.pow(outRadius, 2) - Math.pow(inRadius, 2))
-                                    : 3.14 * (Math.pow(inRadius, 2) - Math.pow(outRadius, 2));
+        return outRadius > inRadius ? Math.PI * (Math.pow(outRadius, 2) - Math.pow(inRadius, 2))
+                                    : Math.PI * (Math.pow(inRadius, 2) - Math.pow(outRadius, 2));
     }
 
 }
