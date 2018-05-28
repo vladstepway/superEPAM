b4e68f0695007bb507038bab844284966f90c836
Vladislav <36796951+vladstepway@users.noreply.github.com>
2018-05-28 19:33:26 +0300

    optimized logic

--- TASK03/ex1/PointOfTriangle.java
+++ TASK03/ex1/PointOfTriangle.java
@@ -1,16 +1,13 @@
 package by.stepovoy.task03.ex1;
 
-public class PointOfTriangle {
+public class PointOfTriangleLogic {
 
     public static boolean isVertextOfTriangle(int x1, int y1, int x2, int y2, int x3, int y3) {
         double AB = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         double BC = Math.sqrt(Math.pow(x3 - x2, 2) + Math.pow(y3 - y2, 2));
         double AC = Math.sqrt(Math.pow(x3 - x1, 2) + Math.pow(y3 - y1, 2));
-        if (AB + BC > AC && AB + AC > BC && AC + BC > AB)
-            return true;
-        else
-            return false;
-    }
+        return AB + BC > AC && AB + AC > BC && AC + BC > AB ;
+  }
 
     public static boolean isOrthogonalTriangle(int x1, int y1, int x2, int y2, int x3, int y3) {
         double AB = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
