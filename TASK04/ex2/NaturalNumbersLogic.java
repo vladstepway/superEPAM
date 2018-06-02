6566fe57f8fc4f6d7bf4e83cd69b2ca97bb3ed95
Vladislav <36796951+vladstepway@users.noreply.github.com>
2018-06-02 20:45:10 +0300

    updated logic

--- TASK04/ex2/NaturalNumbersLogic.java
+++ TASK04/ex2/NaturalNumbersLogic.java
@@ -1,9 +1,12 @@
 package by.stepovoy.task04.ex2;
 
 public class NaturalNumbersLogic {
+    public static int isNatural(int num) {
+        return num < 0 ? -num : num;
+    }
 
     public static int findBiggestDigit(int num) {
-
+        num = isNatural(num);
         int biggest = num % 10;
         int number;
         while (num > 0) {
@@ -16,10 +19,90 @@ public class NaturalNumbersLogic {
         return biggest;
     }
 
-    public static boolean isPalindromNumber(int num) {
+    public static boolean isPalindromeNumber(int num) {
+        num = isNatural(num);
+        int tempNumber = num;
+        int reversibleNumber = 0;
 
-        int firstDigit = num % 10;
-        int lastDigit;
+        while (tempNumber > 0) {
+            reversibleNumber *= 10;
+            reversibleNumber += tempNumber % 10;
+            tempNumber /= 10;
+        }
+        return reversibleNumber == num;
+    }
+
+    public static boolean isSimpleNumber(int num) {
+        num = isNatural(num);
+
+        for (int i = 2; i <= num >>> 1; i++) {
+            if (isDivider(num, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isDivider(int num, int i) {
+        return num % i == 0;
+    }
+
+    public static StringBuffer findSimpleDivisors(int num) {
+        num = isNatural(num);
+        StringBuffer dividers = new StringBuffer();
+        for (int i = 1; i <= num >>> 1; i++) {
+            if (isDivider(num, i)) {
+                if (isSimpleNumber(i)) {
+                    dividers.append(i).append(" ");
+                }
+            }
+        }
+        if (isSimpleNumber(num)) {
+            dividers.append(num).append(" ");
+        }
+
+        return dividers;
+    }
+
+    public static int findGreatestCommonDivisor(int number1, int number2) {
+        number1 = isNatural(number1);
+        number2 = isNatural(number2);
+
+        while (number1 != number2) {
+            if (number1 > number2) {
+                number1 -= number2;
+            } else {
+                number2 -= number1;
+            }
+        }
+        return number1;
+    }
+
+    public static int findLeastCommonMultiple(int number1, int number2) {
+        number1 = isNatural(number1);
+        number2 = isNatural(number2);
+
+        return number1 / findGreatestCommonDivisor(number1, number2) * number2;
+    }
+
+    public static int findDifferentDigitsNumber(int num) {
+        num = isNatural(num);
+
+        int tempNumber;
+        int digits;
+        int count = 0;
+        for (int i = 0; i < 10; i++) {
+            digits = num;
+            while (digits > 0) {
+                tempNumber = digits % 10;
+                if (tempNumber == i) {
+                    count++;
+                    break;
+                }
+                digits /= 10;
+            }
+        }
+        return count;
     }
 }
 
--- TASK04/ex2/TestNumber.java
+++ TASK04/ex2/TestNumber.java
@@ -2,12 +2,30 @@ package by.stepovoy.task04.ex2;
 
 import java.util.Scanner;
 
+import static by.stepovoy.task04.ex2.NaturalNumbersLogic.findLeastCommonMultiple;
+
 public class TestNumber {
     public static void main(String[] args) {
         System.out.println("Enter natural number:");
         Scanner in = new Scanner(System.in);
         int number = in.nextInt();
+        int num2 = in.nextInt();
+
         int maxDigit = NaturalNumbersLogic.findBiggestDigit(number);
         System.out.println(maxDigit);
+
+        System.out.printf("Number %d isPalindrome : %b\n", number, NaturalNumbersLogic.isPalindromeNumber(number));
+
+        System.out.printf("Number %d isSimple : %b\n", number, NaturalNumbersLogic.isSimpleNumber(number));
+
+        StringBuffer stringBuffer;
+        stringBuffer = NaturalNumbersLogic.findSimpleDivisors(number);
+        System.out.println("Dividers of number " + number + ":" + stringBuffer);
+
+        System.out.println("GCD of " + number + " and " + num2 + " is " + NaturalNumbersLogic.findGreatestCommonDivisor(number, num2));
+        System.out.println("LCM of " + number + " and " + num2 + " is " + findLeastCommonMultiple(number, num2));
+
+        System.out.printf("Number %d has %d different digits", number, NaturalNumbersLogic.findDifferentDigitsNumber(number));
+
     }
 }
