ce75ab83009b644bbfab52eee5a6a975f7160a97
Vladislav <36796951+vladstepway@users.noreply.github.com>
2018-06-23 17:22:31 +0300

    edited insertion sort realization

--- TASK07/logic/Sorter.java
+++ TASK07/logic/Sorter.java
@@ -5,13 +5,15 @@ import by.stepovoy.task07.model.container.DynamicArray;
 import by.stepovoy.task07.model.entities.Unit;
 
 public abstract class Sorter implements Sortable {
-    public static void sortByName(DynamicArray array) throws EmptyContainerException {
+    public static void sortByName(DynamicArray array)
+            throws EmptyContainerException {
         if (!isEmptyContainer(array)) {
             new InsertionSort().sort(array);
         }
     }
 
-    private static boolean isEmptyContainer(DynamicArray array) throws EmptyContainerException {
+    private static boolean isEmptyContainer(DynamicArray array)
+            throws EmptyContainerException {
         return array.isEmpty();
     }
 
@@ -29,21 +31,15 @@ public abstract class Sorter implements Sortable {
 class InsertionSort extends Sorter {
     public Unit[] sort(DynamicArray array) {
         Unit[] units = array.getUnits();
-        for (int j = 1; j < array.getSize(); j++) {
-            Unit unit = units[j];
-            int i = j - 1;
-            while (i >= 0
-                    && less(unit.getName(), units[i].getName())) {
-                units[i + 1] = units[i];
-                i--;
+        for (int i = 1; i < units.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (units[j].getHealthPoints()
+                        < units[j - 1].getHealthPoints()) {
+                    swap(units[j], units[j - 1]);
+                }
             }
-            units[i + 1] = unit;
         }
         return units;
-    }
-
-    private static boolean less(String str1, String str2) {
-        return str1.compareTo(str2) < 0;
     }
 }
 
@@ -55,7 +51,8 @@ class BubbleSort extends Sorter {
         while (!change) {
             change = true;
             for (int j = 0; j < units.length - 1; j++) {
-                if (units[j].getDamagePoints() > units[j + 1].getDamagePoints()) {
+                if (units[j].getDamagePoints()
+                        > units[j + 1].getDamagePoints()) {
                     change = false;
                     swap(units[j], units[j + 1]);
                 }
@@ -74,7 +71,8 @@ class SelectionSort extends Sorter {
         for (int i = 0; i < units.length - 1; i++) {
             int index = i;
             for (int j = i + 1; j < units.length; j++)
-                if (units[j].getDamagePoints() < units[index].getDamagePoints())
+                if (units[j].getDamagePoints()
+                        < units[index].getDamagePoints())
                     index = j;
             swap(units[index], units[i]);
         }
