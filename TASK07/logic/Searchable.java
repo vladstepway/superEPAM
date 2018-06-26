e1475d8c78cfbe096ebc8f09ee56e4e55589f0e5
Vladislav <36796951+vladstepway@users.noreply.github.com>
2018-06-26 21:16:55 +0300

    Delete Searchable.java

--- TASK07/logic/Searchable.java
+++
@@ -1,8 +0,0 @@
-package by.stepovoy.task07.logic;
-
-import by.stepovoy.task07.model.container.DynamicArray;
-import by.stepovoy.task07.model.entities.Unit;
-
-public interface Searchable extends Logicable{
-    Unit search(DynamicArray array);
-}
